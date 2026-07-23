9fb91e8729d2aa26e851e47df8a1ebfa60d43fb8
“Nilde <nilde.benedetti@gmail.com>
2026-07-23 18:21:33 +0200

    add: check  of minRating selected value thorugh GET request params; refactor: positioned negative check as first element of if chain in parking variable check for enhancing DRY; feature: added responsive selected field in forms: add: pseudocode logic for min rating form

--- index.php
+++ index.php
@@ -7,11 +7,16 @@
 
     $parking = $_GET['parking'] ??  'all'; // THIS IS CALLED NULL COALESCING OPERATOR
                                            // if key = null a default value is assigned
+    $minScore = $_GET['rating'] ?? 'all';
     // save filtered list based on request parameters
+
+    var_dump($minScore);
 
     $filteredHotels = null;
 
-    if ( $parking !== 'all' ) {
+    if ( $parking === 'all' ) {
+        $filteredHotels = $hotels;
+    } else {
         // SE FILTRO PARKING === SI
         if ( $parking === 'yes')
             // devo esaminare ciascun hotel con foreach
@@ -37,7 +42,23 @@
                 }
             }
         }
-    }
+    } // ELSE FILTERED HOTELS === TUTTI HOTEL MA MEGLIO EMTTO PRIMA QUESTA CONDIZIONE
+
+    var_dump($filteredHotels);
+
+    // New list for hotels con min rating
+    // SE IL MIN RATING È < RATING SELECTED
+        // ALLORA CONTINUE - COSI SALTA ESECUZIONE RESTO CODICE
+    // SE MIN RATING >= RATING SELECTED
+        // controlla se current element è gia nella lista
+                // SE NO continue
+                // SE SI rimuovilo
+                // se uso UNSET resta il buco
+                // se no devo fare la SPLICE ma mi serve index
+                // facciamo la splice e usiamo la foreach per cercare index usando la $key come valore da assegnare a $index quando i valori matchano
+
+
+
 
 ?>
 <!DOCTYPE html>
@@ -59,18 +80,51 @@
     <section class="form p-4">
         <!--- keep in mind GET as form method is vital here!!! --->
         <form method="get" action="index.php">
-        <div class="select-wrapper">
+        <div class="select-wrapper d-flex align-items-caseline">
             <!--- parking select -->
             <div class="parking-select-wrapper p-3">
                 <label for="parking-select" class="py-2">Parking</label>
                 <select class="form-select" id="parking-select" name="parking">
-                    <option value="yes">Yes</option>
-                    <option value="no">No</option>
-                    <option value="all" selected>Show all</option>
+                    <option value="yes" 
+                    <?php echo $parking === "yes" ? "selected" :  "";  ?>
+                        >Yes</option>
+                    <option value="no"
+                    <?php echo $parking === "no" ? "selected" :  "";  ?>
+                    >No</option>
+                    <option value="all" 
+                    <?php echo $parking === "all" ? "selected" :  "";  ?>
+                    >Show all</option>
                 </select>
             </div>
+            <!--- star rating select -->
+            <div class="parking-select-wrapper p-3">
+                <label for="parking-select" class="py-2">Minimum Rating Acceptable</label>
+                <select class="form-select" id="rating-select" name="rating">
+                    <option value="1" 
+                    <?php echo $rating === "1" ? "selected" :  "";  ?>
+                        >1</option>
+                    <option value="2"
+                    <?php echo $rating === "2" ? "selected" :  "";  ?>
+                    >2</option>
+                    <option value="3"
+                    <?php echo $rating === "3" ? "selected" :  "";  ?>
+                    >3</option>
+                    <option value="4"
+                    <?php echo $rating === "4" ? "selected" :  "";  ?>
+                    >4</option>
+                    <option value="5"
+                    <?php echo $rating === "5" ? "selected" :  "";  ?>
+                    >5</option>
+                    <option value="all"
+                    <?php echo $rating === "all" ? "selected" :  "";  ?>
+                    >Show all</option>
+                </select>
+            </div>
+            <div class="btn-container d-flex align-items-center pt-3">
+                <button type="submit" class="btn btn-dark">Refine</button>
+            </div>   
         </div>
-        <button type="submit" class="btn btn-primary">Refine</button>
+
         </form>
     </section>
     <section class="hotels-table">
